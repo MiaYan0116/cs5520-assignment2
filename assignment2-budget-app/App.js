f1b018251088d8fbd4618d95b14ff737523c82c9
mia <yanjiaxin96@gmail.com>
2023-10-19 21:42:50 -0700

    modified styling of tab bar

--- assignment2-budget-app/App.js
+++ assignment2-budget-app/App.js
@@ -4,33 +4,24 @@ import AllExpensesScreen from './screens/AllExpensesScreen';
 import InputScreen from './screens/InputScreen';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const item1 = {
-  name: "item1",
-  quantity: 2,
-  price: 100,
-  isOverBudget: false
-}
-const item2 = {
-  name: "item2",
-  quantity: 3,
-  price: 200,
-  isOverBudget: true
-}
-function BottomTabNavigator() {
-  return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={AllExpensesScreen} initialParams={{itemsArray:[item1, item2]}} />
-      <Tab.Screen name="Overbudget" component={AllExpensesScreen} initialParams={{itemsArray:[item2]}} />
-    </Tab.Navigator>
-  );
-}
+
+
 
 export default function App() {
-  
+  const item1 = {
+    name: "item1",
+    quantity: 2,
+    price: 100,
+    isOverBudget: false
+  }
+  const item2 = {
+    name: "item2",
+    quantity: 3,
+    price: 200,
+    isOverBudget: true
+  }
   return(
     <NavigationContainer>
       <Stack.Navigator 
@@ -43,11 +34,11 @@ export default function App() {
       >
       <Stack.Screen 
           name='All Expenses' 
-          component={BottomTabNavigator}
+          component={AllExpensesScreen}
           options={{
             title: 'All Expenses',
           }}
-          initialParams={{ itemsArray: [item1, item2] }}
+          initialParams={{ itemsArray: [item1, item2], screenType: 'all' }}
       />
       <Stack.Screen 
           name='Overbudget Expenses' 
@@ -55,7 +46,7 @@ export default function App() {
           options={{
             title: 'Overbudget Expenses',
           }}
-          initialParams={{ itemsArray: [item2] }}
+          initialParams={{ itemsArray: [item2], screenType: 'over' }}
       />
       <Stack.Screen 
           name='Add an Expense' 
@@ -67,14 +58,6 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   )
-
-  // return (
-  //   <View style={styles.container}>
-  //     {/* <AllExpensesScreen itemsArray={[item1, item2]}/> */}
-  //     <InputScreen/>
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
 }
 
 const styles = StyleSheet.create({
--- assignment2-budget-app/screens/AllExpensesScreen.js
+++ assignment2-budget-app/screens/AllExpensesScreen.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native'
 import SingleItem from '../components/SingleItem'
+import { FontAwesome } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 const AllExpensesScreen = ({ navigation, route }) => {
 	const itemsArray = route.params.itemsArray;
-	console.log(route.params);
-	
+	const screenType = route.params.screenType;
+
 	React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -19,22 +21,67 @@ const AllExpensesScreen = ({ navigation, route }) => {
     });
   }, [navigation]);
   return(
-		<FlatList
-			contentContainerStyle={styles.allExpensesContainer}
-			data={itemsArray}
-			renderItem={({item}) => {
-				return(
-					<SingleItem singleItem={item}/>
-				)
-			}}
-		/>
+		<View style={styles.container}>
+			<FlatList
+				contentContainerStyle={styles.allExpensesContainer}
+				data={itemsArray}
+				renderItem={({item}) => {
+					return(
+						<SingleItem singleItem={item}/>
+					)
+				}}
+			/>
+			<View style={styles.tabButtonContainer}>
+				<TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+					{screenType === 'all' ? (
+						<FontAwesome name="home" size={24} color="yellow" />
+					): (
+						<FontAwesome name="home" size={24} color="black" />
+					)}
+					<Text style={styles.buttonText}>Home</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {navigation.push("Overbudget Expenses")}} style={styles.button}>
+        	{screenType === 'over' ? (
+          	<AntDesign name="exclamation" size={24} color="yellow" />
+        	) : (
+          	<AntDesign name="exclamation" size={24} color="black" />
+        	)}
+        	<Text style={styles.buttonText}>Overbudget</Text>
+      	</TouchableOpacity>
+			</View>
+		</View>
+		
 	)
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
 	allExpensesContainer: {
+		flex: 9,
 		marginTop: 30,
 		alignItems: 'center',
+	},
+	tabButtonContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		paddingHorizontal: 80,
+	},
+	button:{
+		width: '100%',
+		height: '20%',
+		backgroundColor: '#483D8B',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	buttonText: {
+		color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
 	}
 })
 
